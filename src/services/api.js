e5ac69a367a5a100d4193f4358e2d8db66aa616f
Diego Scavone <55470939+diegoscavone@users.noreply.github.com>
2024-08-29 20:16:43 -0400

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: 'https://foodexplorer-api-kaol.onrender.com:3333',
+  baseURL: 'https://foodexplorer-api-kaol.onrender.com',
   withCredentials: true
 })
